bf4c768634f6b7877a91368d6334a287990f42d9
Chris Anderton <cga@heroku.com>
2017-06-06 17:44:48 +0100

    moo

--- src/test/java/com/sky/test/TestRunner.java
+++ src/test/java/com/sky/test/TestRunner.java
@@ -4,10 +4,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-System.setProperty("webdriver.chrome.driver", "/app/.apt/usr/bin/google-chrome");
-
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "src/test/resources/features/first.feature" }, glue = {
 		"com.sky.test.stepDefinitions" }, tags = { "@test" })
 public class TestRunner {
+  System.setProperty("webdriver.chrome.driver", "/app/.apt/usr/bin/google-chrome");
 }
